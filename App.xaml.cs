950dbd6e91057811824711f4204c09e5fbb03da0
AndIx11 <andrew.animod@mail.ru>
2024-09-18 01:54:45 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using DatabaseManagers;
+using ModelViewSystem;
+using System;
+using System.Reflection;
+using System.Windows;
+
+namespace СargoTransportationProject
+{
+	/// <summary>
+	/// Логика взаимодействия для App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		#region WINDOWS
+
+		private void OpenWindow(Type windowType, ViewModelBase viewModel)
+		{
+			Assembly assembly = windowType.Assembly;
+			Type type = assembly.GetType(windowType.FullName);
+
+			Window window = (Window)Activator.CreateInstance(type);
+			window.ResizeMode = ResizeMode.NoResize;
+
+			window.DataContext = viewModel;
+			window.ShowDialog();
+		}
+
+		private void OpenCatalogEditWindow(CatalogEditModelView modelView)
+		{
+			try
+			{
+				CatalogEditWindow editWindow = new CatalogEditWindow(modelView);
+				editWindow.ShowDialog();
+			}
+			catch
+			{}
+		}
+		 
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			WindowEvents.OnLogin += (UserModel user) => (new MainWindow(user)).Show();
+			WindowEvents.OnLogOut += () => (new LoginWindow()).Show();
+			WindowEvents.OnOpenWindow += OpenWindow;
+			WindowEvents.OnOpenCatalogEdit += OpenCatalogEditWindow;
+		}
+
+		#endregion
+	}
+}
